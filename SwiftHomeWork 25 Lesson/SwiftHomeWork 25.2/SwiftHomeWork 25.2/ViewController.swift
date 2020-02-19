816c8af6591cfcbda7a7739f204d52baaed880d6
Maxim Tsalyuk <max.tsalyuk@gmail.com>
2020-02-19 17:59:24 +0200

    SwiftHomeWork 25 Done

---
+++ SwiftHomeWork 25 Lesson/SwiftHomeWork 25.2/SwiftHomeWork 25.2/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  SwiftHomeWork 25.1
+//
+//  Created by Максим on 2/17/20.
+//  Copyright © 2020 Максим. All rights reserved.
+//
+
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        AF.request("https://httpbin.org/get").response { response in
+            debugPrint(response)
+        }
+        
+        let dict = ["1":2, "2":"two", "3": nil] as [String: Any?]
+        let json = JSON(dict)
+        let representation = json.rawString([.castNilToNSNull: true])
+        print(representation!)
+    }
+    
+    
+}
+
+
